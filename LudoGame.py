056f4854d83c8ee1e49a6ca0978d06900d146621
Jeffrey Wang <33505593+MorePie@users.noreply.github.com>
2022-07-23 23:52:33 -0700

    Skeleton of the base functions/classes required for the assignment.

---
+++ LudoGame.py
@@ -0,0 +1,34 @@
+# Author: Jeffrey Wang
+# GitHub username: Jeffrey-Wang98
+# Date: July 23, 2022
+# Description:
+
+class Player:
+    def __init__(self):
+        pass
+
+    def get_completed(self):
+        pass
+
+    def get_token_p_step_count(self):
+        pass
+
+    def get_token_q_step_count(self):
+        pass
+
+    def get_space_name(self):
+        pass
+
+
+class LudoGame:
+    def __init__(self):
+        pass
+
+    def get_player_by_position(self, player_position):
+        pass
+
+    def move_token(self, player, token, steps):
+        pass
+
+    def play_game(self, players_list, turns_list):
+        pass
